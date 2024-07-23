f0c4d98a5ba5088e77c1a609221722a9d627276a
sofia-fal <sofia-fal@hotmail.com>
2024-07-23 15:08:07 +0200

    Première création du Slideshow

--- src/components/logements-page.jsx
+++ src/components/logements-page.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import logementdata from '../data/logements.json'
+import Slideshow from './slideshow'
 import Error from './error'
 
 function Logement() {
-    const { id } = useParams()
-    const [logement, setLogement] = useState(null)
-    const navigate = useNavigate()
+  const { id } = useParams()
+  const [logement, setLogement] = useState(null)
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    const clickedCard = logementdata.find((logement) => logement.id === id)
+    if (!clickedCard) {
+      navigate('*')
+    } else {
+      setLogement(clickedCard)
+    }
+  }, [id, navigate])
 
-    useEffect(() => {
-        const clickedCard = logementdata.find(logement => logement.id === id)
-        if (!clickedCard) {
-            navigate('*')
-        } else {
-            setLogement(clickedCard)
-        }
-    }, [id, navigate]
-)
-
-if (!logement) {
+  if (!logement) {
     return <Error />
-}
+  }
 
   return (
-    <div></div>
-  )
+    <div>
+      <h1>{logement.title}</h1>
+      <Slideshow images={logement.pictures} />
+    </div>
+  );
 }
 
 export default Logement

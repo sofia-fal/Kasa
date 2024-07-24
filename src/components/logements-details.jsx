9c90bbf980aa9080c857926eb319e34024a414da
sofia-fal <sofia-fal@hotmail.com>
2024-07-24 15:48:33 +0200

    Fin de mise en page de la page de logement

--- src/components/logements-details.jsx
+++ src/components/logements-details.jsx
@@ -1,33 +1,53 @@
 import React from 'react'
 import '../style/logement-details.css'
+import Rating from './rating'
+import Collapse from './collapse'
 
 function Details({ logement }) {
-    const { title, description, host, rating, location, equipments, tags } = logement;
+    const { title, description, host, rating, location, equipments, tags } = logement
 
-  return (
-    <div>
-        <div className='detail-header'>
-            <h2>{title}</h2>
-            <p className='location'>{location}</p>
-            <ul className='tags'>
-                {tags.map((tag, index) => (
-                    <li key={index} className='tag'>{tag}</li>
-                ))}
-            </ul>
+    return (
+        <div className='wrapper'>
+            <div className='details'>
+                <div className='basic-info'>
+                    <h2>{title}</h2>
+                    <p className='location'>{location}</p>
+                    <ul className='tags'>
+                        {tags.map((tag, index) => (
+                            <li key={index} className='tag'>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='host-section'>
+                    <div className='host'>
+                    <p>{host.name}</p>
+                    <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <clipPath id="clip-circle">
+                                <circle cx="32" cy="32" r="32" />
+                            </clipPath>
+                        </defs>
+                        <circle cx="32" cy="32" r="32" fill="#C4C4C4" />
+                        <image href={host.picture} x="0" y="0" height="64" width="64" clipPath="url(#clip-circle)" />
+                    </svg>
+                    </div>
+                    <div className='rating'>
+                        <Rating rating={rating} />
+                    </div>
+                </div>
+            </div>
+            <div className='desc-equip'>
+                <Collapse title="Description" content={description} />
+                <Collapse title="Equipments" content={
+                    <ul>
+                        {equipments.map((equipment, index) => (
+                            <li key={index}>{equipment}</li>
+                        ))}
+                    </ul>
+                } />
+            </div>
         </div>
-        
-        <p>Description: {description}</p>
-        <p>Host: {host.name}</p>
-        <p>Rating: {rating}</p>
-        <p>Location: {location}</p>
-        <p>Equipments:</p>
-        <ul>
-            {equipments.map((equipment, index) => (
-                <li key={index}>{equipment}</li>
-            ))}
-        </ul>
-    </div>
-  )
+    )
 }
 
 export default Details
